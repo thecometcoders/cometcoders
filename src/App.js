0e80aaf5a118d160cb12f4c327f2cd498a9c80c7
Andrew Heyer <andrewbheyer@gmail.com>
2023-08-01 10:29:02 -0400

    style changes, MUI updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,36 @@ import {createTheme, ThemeProvider} from "@mui/material";
 const theme = createTheme({
   spacing: 20,  // spacing in px
   /* other theme customizations */
+  palette: {
+    primary: {
+      main: "rgba(35, 168, 209, 1)"
+    }
+  },
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        primary: {
+          fontFamily: "orbitron-black",
+          fontSize: "3em"
+        },
+        subheading: {
+          fontFamily: "orbitron-black",
+          fontSize: "1em"
+        }
+      }
+    },
+    MuiDivider: {
+      styleOverrides: {
+        root: {
+          padding: '16px 0',
+        },
+      },
+    },
+  },
+
+
+
+
 });
 
 
--- src/components/AccountabilityMeetings/accountability-meetings.component.jsx
+++ src/components/AccountabilityMeetings/accountability-meetings.component.jsx
@@ -1,6 +1,4 @@
-
-
-import { collection, getDocs } from "firebase/firestore";
+import {collection, getDocs} from "firebase/firestore";
 import {db} from '../../firebase.js';
 import {useEffect, useState} from "react";
 
@@ -18,76 +16,110 @@ import {Box, Container, Divider, Stack, Typography} from "@mui/material";
 const AccountabilityMeetings = () => {
 
   const [meetingNotes, setMeetingNotes] = useState([]);
+  const [totalPosts, setTotalPosts] = useState([])
+
+  useEffect(() => {
+    const count = countPosts(meetingNotes);
+    setTotalPosts(count);
+  }, [meetingNotes]);
+
+  function countPosts(months) {
+    let count = 0;
+    months.forEach(month => {
+      count += month.posts.length;
+    });
+    return count;
+  }
 
   const fetchPost = async () => {
     await getDocs(collection(db, "meeting-notes"))
-      .then((querySnapshot)=>{
-        const newData = querySnapshot.docs
-          .map((doc) => (doc.data()));
+      .then((querySnapshot) => {
+        const newData = querySnapshot.docs.map((doc) => (doc.data()));
+
+        // Sort months
+        newData.sort((a, b) => new Date(b.month) - new Date(a.month));
+
+        // Sort posts in each month
+        newData.forEach(month => {
+          month.posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+        });
+
         setMeetingNotes(newData);
-        console.log(newData);
       })
   }
-  useEffect(()=>{
+  useEffect(() => {
     fetchPost();
   }, [])
-
   useEffect(() => {
     addCollectionAndDocuments('meeting-notes', MEETING_NOTE_DATA)
-  },[])
+  }, [])
 
+
+  // Render a loading screen when the post data has not arrive yet
   if (!meetingNotes.length) return <h2>Loading....</h2>;
 
   return (
 
-    <Container >
+    <Container sx={{width: '100%'}} >
 
-      <div>
+      <Box>
         {meetingNotes.map(month => {
           if (!month) return null;
           return (
             <div>
-              <Divider><Typography variant="body2" color="text.secondary">{month.month}</Typography></Divider>
-              <Stack spacing={2}>
-              {month.posts
-                .filter(post => post && post.date)
-                .map(post => {
-                  if (!post) return null;
-                  return (
-                    <Card >
-                      <CardHeader title={post.date} />
-                      {post.image && <CardMedia component="img" image={post.image} /> }
-                      <CardContent>
-                        <Typography variant="body2" color="text.secondary">
-                          {post.notes}
-                        </Typography>
-                        {post.attendees
-                          .filter(attendee => attendee && attendee.name)
-                          .map(attendee => {
-                            if (!attendee) return null;
-                            return (
-                              <>
-                                <Typography variant="h6">
-                                  {attendee.name}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                  Accomplished: {attendee.accomplishments && attendee.accomplishments.join(", ")}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                  Committing to: {attendee.commitments && attendee.commitments.join(", ")}
-                                </Typography>
-                              </>
-                            )
-                          })}
-                      </CardContent>
-                    </Card>
-                  )
-                })}
+              <Divider sx={{
+                "&::before, &::after": {
+                  borderColor: "primary.main",
+                },
+              }}><Typography variant="primary" color={"primary.main"}>{new Date(month.month).toLocaleString('en', { month: 'long' })}</Typography></Divider>
+              <Stack spacing={2} alignItems={"center"}>
+                {month.posts
+                  .filter(post => post && post.date)
+                  .map((post, index) => {
+                    if (!post) return null;
+                    return (
+
+                      <Card
+                        sx={{maxWidth: 600}}
+                      >
+                        <CardHeader title={
+                          <><Typography variant="subheading">Comet Log #{totalPosts-index}</Typography>
+                          <Typography variant="body2">{post.date}</Typography></>
+                        }/>
+
+
+                        {post.image && <CardMedia component="img" image={post.image}/>}
+                        <CardContent>
+                          <Typography variant="body2" color="text.secondary">
+                            {post.notes}
+                          </Typography>
+                          {post.attendees
+                            .filter(attendee => attendee && attendee.name)
+                            .map(attendee => {
+                              if (!attendee) return null;
+                              return (
+                                <>
+                                  <Typography variant="subheading">
+                                    {attendee.name}
+                                  </Typography>
+                                  <Typography variant="body2" color="text.secondary">
+                                    Accomplished: {attendee.accomplishments && attendee.accomplishments.join(", ")}
+                                  </Typography>
+                                  <Typography variant="body2" color="text.secondary">
+                                    Committing to: {attendee.commitments && attendee.commitments.join(", ")}
+                                  </Typography>
+                                </>
+                              )
+                            })}
+                        </CardContent>
+                      </Card>
+                    )
+                  })}
               </Stack>
             </div>
           )
         })}
-      </div>
+      </Box>
 
     </Container>
 
